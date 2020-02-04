f14e4360896d63aabf6cfc54bcac46b928df7cdf
Radulescu Vlad <vladulesq@gmail.com>
2020-02-04 19:24:34 +0200

    first commit

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public float force = 100f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+            gameObject.GetComponent<Rigidbody>().AddForce(transform.forward * force);
+
+        if (Input.GetKey(KeyCode.S))
+            gameObject.GetComponent<Rigidbody>().AddForce(-transform.forward * force);
+
+        if (Input.GetKey(KeyCode.A))
+            gameObject.GetComponent<Rigidbody>().AddForce(-transform.right * force);
+
+        if (Input.GetKey(KeyCode.D))
+            gameObject.GetComponent<Rigidbody>().AddForce(transform.right * force);
+    }
+}
